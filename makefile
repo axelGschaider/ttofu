c0e3a040419ef2825f4b10d280d187560b9d579e
Axel Gschaider <axel.gschaider@gmail.com>
2012-10-06 01:18:12 +0200

    more tests

--- makefile
+++ makefile
@@ -4,10 +4,12 @@ all:
 	cabal-dev build
 
 
-test:
+tests:
 	cabal-dev configure --enable-tests
 	cabal-dev build
 	cabal-dev test
 
 
+clean:
+	cabal-dev clean
 
--- test/Test.hs
+++ test/Test.hs
@@ -8,17 +8,40 @@ import Test.QuickCheck
 import Test.HUnit
 
 import SplitUtil
-import DataStructuresUtil
 
 
 case_onlyOneLine = split (stupidSlicer 10) "123" @?= ["123"]
+case_twoLines = split (stupidSlicer 2) "123" @?= ["12", "3"]
 
+prop_correctNumberOfLines i s = 
+  (i > 0) ==> slicesLength  == expectedLength
+  where slices = split (stupidSlicer i) s
+	slicesLength = length slices
+	expectedLength = (length s) /-% i  
+        types = (i::Int, s::String)
+
+prop_reversable i s =
+  (i > 0) ==> recombined == s
+    where sliced = split (stupidSlicer i) s
+          recombined = foldl (++) "" sliced
 
 tests = [
-  testGroup "simple cases" [
-    testCase "one line" case_onlyOneLine
+  testGroup "stupid slicer cases" [
+    testCase "one line" case_onlyOneLine,
+    testCase "two lines" case_twoLines
+    ],
+  testGroup "stupid slicer properties" [
+    testProperty "correct number of lines" prop_correctNumberOfLines,
+    testProperty "splittins is reversable" prop_reversable
     ]
   ]
 
 runTests = defaultMain tests
+
+
+(/-%) :: Int -> Int -> Int
+a /-% b = base + rest
+  where base = a `div` b
+        m = a `mod` b
+	rest = if (m == 0) then 0 else 1
 
